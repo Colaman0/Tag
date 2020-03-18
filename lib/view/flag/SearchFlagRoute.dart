5d0e485b4ccc122ba3be39cb87392381472e7ce0
kyle <845879469@qq.com>
2020-03-18 18:05:25 +0800

    feat:完成搜索flag页面

--- lib/view/flag/SearchFlagRoute.dart
+++ lib/view/flag/SearchFlagRoute.dart
@@ -37,9 +37,11 @@ class SearchFlagRoute extends SearchDelegate {
     return FutureBuilder<List<BuildFlagInfo>>(
       initialData: [
         BuildFlagInfo(
-            flagName: "林世杰生日",
+            flagName: "距离林世杰生日",
             date: DateTime.now(),
-            categories: ["知识", "改变", '命运'])
+            categories: ["生日", "自己"]),
+        BuildFlagInfo(
+            flagName: "距离林XX生日", date: DateTime.now(), categories: ["恩啊"])
       ],
       builder: (context, data) {
         List<BuildFlagInfo> infos = data.data;
@@ -56,22 +58,27 @@ class SearchFlagRoute extends SearchDelegate {
                     Positioned(
                       left: 0,
                       right: 0,
-                      top: DP.toDouble(108),
+                      top: DP.toDouble(110),
                       child: Wrap(
                         runAlignment: WrapAlignment.start,
                         children: info.categories
                             .map((tagName) => Card(
-                                child: TextView(
-                                  tagName,
-                                  textColor: Colors.white,
-                                )
-                                    .aligment(null)
-                                    .backgroundColorStr(Constants.COLOR_BLUE)
-                                    .corner(leftBottom: 5, rightBottom: 5)
-                                    .padding(
-                                        left: 12, right: 12, top: 8, bottom: 8)
-                                    .size(width: View.WRAP, height: View.WRAP),
-                                elevation: 8))
+                                  child: TextView(
+                                    tagName,
+                                    textColor: Colors.white,
+                                  )
+                                      .aligment(null)
+                                      .backgroundColorStr(Constants.COLOR_BLUE)
+                                      .corner(leftBottom: 5, rightBottom: 5)
+                                      .padding(
+                                          left: 12,
+                                          right: 12,
+                                          top: 8,
+                                          bottom: 8)
+                                      .size(
+                                          width: View.WRAP, height: View.WRAP),
+                                  elevation: 5,
+                                ))
                             .toList(),
                         spacing: DP.toDouble(8),
                       ),
@@ -83,31 +90,105 @@ class SearchFlagRoute extends SearchDelegate {
                         child: View(
                           child: Card(
                             elevation: 5,
-                            child: Column(
+                            child: Row(
                               mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
                               children: <Widget>[
-                                buildTitle(info),
-                                Flexible(
-                                  flex: 1,
-                                  child: buildFlagNameWidget(info),
+                                Expanded(
+                                  child: Column(
+                                    children: <Widget>[
+                                      Row(
+                                        children: <Widget>[
+                                          View(
+                                            child: Icon(Icons.today,color: HexColor(Constants.COLOR_BLUE),size: DP.toDouble(26),),
+                                          ).margin(left: 12),
+                                          TextView("2019年3月7日",
+                                                  textSize: 24,
+                                                  textColorStr:
+                                                      Constants.COLOR_BLUE)
+                                              .aligment(Alignment.centerLeft)
+                                              .margin(left: 12, right: 12),
+                                        ],
+                                      ),
+                                      Divider(
+                                        height: 1,
+                                        color: HexColor(Constants.COLOR_BLUE),
+                                      ),
+                                      Expanded(
+                                        child: TextView(info.flagName + "还有",
+                                                textSize: 30,
+                                                textColorStr:
+                                                    Constants.MAIN_COLOR)
+                                            .aligment(Alignment.centerLeft)
+                                            .margin(left: 12, right: 12),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                                Flexible(
-                                  flex: 1,
-                                  child: buildFlagNameWidget(info),
-                                ),
+                                View(
+                                  child: buildDayText("123"),
+                                )
+                                    .size(width: 120, height: View.MATCH)
+                                    .backgroundColorStr(Constants.COLOR_BLUE)
                               ],
                             ),
                           ),
-                        ).size(width: View.MATCH, height: 200)),
+                        ).size(width: View.MATCH, height: 120)),
                   ],
                 ))
                     .click(() {})
-                    .size(width: View.MATCH, height: 200)
+                    .size(width: View.MATCH, height: 160)
                     .margin(both: 12);
               }),
         ).backgroundColorStr("#F5F5F5");
       },
+    );
+  }
+
+  Widget buildDayText(String text) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      textBaseline: TextBaseline.alphabetic,
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      children: <Widget>[
+        Expanded(
+          child: LayoutBuilder(
+            builder: (builder, box) {
+              double fontSize = calculateAutoscaleFontSize(
+                  text,
+                  GoogleFonts.rubik(),
+                  SP.get(28),
+                  box.maxWidth - DP.toDouble(12));
+              return Container(
+                child: Baseline(
+                  child: Text(text,
+                      softWrap: false,
+                      style: GoogleFonts.rubik(
+                        textStyle:
+                            TextStyle(color: Colors.white, fontSize: fontSize),
+                      )),
+                  baselineType: TextBaseline.alphabetic,
+                  baseline: DP.toDouble(50),
+                ),
+                margin: EdgeInsets.only(
+                    left: DP.toDouble(12), right: DP.toDouble(4)),
+              );
+            },
+          ),
+        ),
+        Padding(
+          child: Baseline(
+            child: Text("天",
+                style: GoogleFonts.rubik(
+                  textStyle:
+                      TextStyle(color: Colors.white, fontSize: SP.get(20)),
+                )),
+            baseline: DP.toDouble(50),
+            baselineType: TextBaseline.alphabetic,
+          ),
+          padding: EdgeInsets.only(right: 8),
+        )
+      ],
     );
   }
 
@@ -117,21 +198,50 @@ class SearchFlagRoute extends SearchDelegate {
             Icon(Icons.calendar_today,
                 color: Colors.white, size: DP.toDouble(24)),
             TextView(
-              "${info.date.year}.${info.date.month}月${info.date.day}日 - ${info.date.hour}时${info.date.minute}分",
+              "${info.date.year}.${info.date.month}.${info.date.day}",
               textSize: 26,
-              textColor: Colors.white,
+              textColor: Colors.black,
             ).margin(left: 12)
           ],
         ),
       )
           .corner(leftTop: 10, rightTop: 10)
           .padding(both: 16)
-          .aligment(Alignment.centerLeft)
-          .backgroundColorStr(Constants.MAIN_COLOR);
+          .aligment(Alignment.centerLeft);
 
   Widget buildFlagNameWidget(BuildFlagInfo info) => TextView(
         info.flagName,
         textSize: 30,
         textColor: HexColor(Constants.MAIN_COLOR),
       );
+
+  bool IsExpansion(String text, BoxConstraints constraints, fontSize) {
+    TextPainter _textPainter = TextPainter(
+        maxLines: 1,
+        text: TextSpan(
+            text: text,
+            style: TextStyle(color: Colors.black, fontSize: fontSize)),
+        textDirection: TextDirection.ltr)
+      ..layout(maxWidth: constraints.maxWidth, minWidth: constraints.minWidth);
+    return _textPainter.didExceedMaxLines;
+  }
+
+  double calculateAutoscaleFontSize(
+      String text, TextStyle style, double startFontSize, double maxWidth) {
+    final textPainter = TextPainter(textDirection: TextDirection.ltr);
+
+    var currentFontSize = startFontSize;
+
+    while (true) {
+      final nextFontSize = currentFontSize + 1;
+      final nextTextStyle = style.copyWith(fontSize: nextFontSize);
+      textPainter.text = TextSpan(text: text, style: nextTextStyle);
+      textPainter.layout();
+      if (textPainter.width >= maxWidth) {
+        return currentFontSize;
+      } else {
+        currentFontSize = nextFontSize;
+      }
+    }
+  }
 }
